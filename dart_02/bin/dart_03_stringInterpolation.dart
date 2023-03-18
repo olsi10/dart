9ef3706a22b1bb586c183d954d082144c3050970
olsi10 <wangbampang2035@gmail.com>
2023-03-18 19:53:31 +0900

    $ 뒤에 변수명 붙이면 문자열 붙이기 계산식은 ${ } -> 중괄호 붙이기

---
+++ dart_02/bin/dart_03_stringInterpolation.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = 'nico';
+  // $ 뒤에 변수명
+  // 계산식은 중괄호로 묶어야 함
+  var age = 10;
+  var greeting = 'hello my name is $name, nice to meet you and im ${age + 2}';
+  print(greeting);
+}
