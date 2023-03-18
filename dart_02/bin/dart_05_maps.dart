e7f23ad37431c774dd7b07da88ef9d8c0d105943
olsi10 <wangbampang2035@gmail.com>
2023-03-19 03:03:07 +0900

    map of dart

---
+++ dart_02/bin/dart_05_maps.dart
@@ -0,0 +1,28 @@
+void main() {
+  // dart의 map은 js, ts의 object
+  // pythno의 dictionary
+  // 키와 값으로 이루어
+  // 각 키는 한 번만 발생하지만 동일한 값을 여러 번 사용할 수 있음
+
+  // var player = {
+  //   'name': 'nico',
+  //   'xp': 19.99,
+  //   'supper': false,
+  // };
+
+  // Map<int, bool> player = {
+  //   // 1: true,
+  //   // 2: false,
+  //   // 3: true,
+  // }
+
+  // Map<List<int>, bool> player = {
+  //   [1, 2, 3, 5]: true,
+  // };
+
+  // Map 생성자를 사용하여 동일한 객체를 만들 수 있습니다.
+  var gifts2 = Map();
+  gifts2['first'] = 'partridge';
+  gifts2['second'] = 'turtledoves';
+  gifts2['fifth'] = 'golden rings';
+}
