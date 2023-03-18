e4c56c7c6e30a3fd165cdf0d837196981cfa2680
olsi10 <wangbampang2035@gmail.com>
2023-03-19 03:07:12 +0900

    set of dart use { }

---
+++ dart_02/bin/dart_06_sets.dart
@@ -0,0 +1,14 @@
+void main() {
+  // python의 tuple과 동일
+  var number = {1, 2, 3, 4};
+  // Set<int> number = {1,2,3,4};
+  number.add(1);
+  number.add(1);
+  number.add(1);
+  number.add(1);
+  number.add(1);
+  number.add(1);
+  number.add(1);
+
+  print(number); // {1, 2, 3, 4}
+}
